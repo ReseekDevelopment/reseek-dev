db77d7cea872e1e0998e48083d9c0988be8c592a
ReseekDevelopment <analytics.reseek@gmail.com>
2023-03-21 11:48:25 +0100

    update

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const parceled = true;
 //Scroll smooth
 const lenis = new Lenis({
   duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
   direction: 'vertical', // vertical, horizontal
   gestureDirection: 'vertical', // vertical, horizontal, both
   smooth: true,
+  mouseMultiplier: 1,
   smoothTouch: false,
   touchMultiplier: 2,
   infinite: false,
 })
 
-// Get scroll value. This is just for testing purposes. Delete this if you're not using the scroll value for anything.
+//get scroll value
 lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
   console.log({ scroll, limit, velocity, direction, progress })
 })
@@ -23,38 +24,16 @@ function raf(time) {
   requestAnimationFrame(raf)
 }
 
-// Grab all elements that have a "data-target" attribute
-const scrollButtons = document.querySelectorAll('[data-target]');
-
-// For each element, listen to a "click" event
-scrollButtons.forEach(button => {
-  button.addEventListener('click', e => {
-    e.preventDefault();
-
-    // get the DOM element by the ID (data-target value)
-    var target = button.dataset.target,
-        $el = document.getElementById(target.replace('#', ''));
-
-    // Use lenis.scrollTo() to scroll the page to the right element
-    lenis.scrollTo($el, {
-      offset: 0, 
-      immediate: false,
-      duration: 3,
-      easing: (x) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2), // https://easings.net
-    });
-  });
-});
-
+function checkScrollMenu(){
+if($('.menu-wrapper').hasClass('open')){
+      lenis.stop();
+    } else {
+     lenis.start();
+    }
+}
+checkScrollMenu();
 requestAnimationFrame(raf)
 
-function checkScrollMenu(){
-  if($('.menu-wrapper').hasClass('open')){
-        lenis.stop();
-      } else {
-       lenis.start();
-      }
-  }
-  checkScrollMenu();
 //Typed text automatic
   var typed2 = new Typed('#typed', {
     strings: ['Web Solutions' ,'Web Development', 'Web Design'],
@@ -159,7 +138,7 @@ ScrollTrigger.defaults({
   markers: false
 });
 
-ScrollTrigger.normalizeScroll(true);
+
 
 
 //round section 1 animation
